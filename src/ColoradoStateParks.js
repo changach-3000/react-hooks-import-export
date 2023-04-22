ef9cef6db82df403a873a8bf25cf1c0db9211012
changach-3000 <sharon.changach@student.moringaschool.com>
2023-04-23 00:09:43 +0300

    Shows in browser

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,14 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import {elevation, trees,wildlife} from "./parks/RockyMountain";
+
 
 function ColoradoStateParks() {
-  howManyParks(); // => "42 parks!"
+  howManyParks() // => "42 parks!"
+  console.log(trees);
+  wildlife()
+  elevation()
 
   return <h1>Colorado State Parks!</h1>;
 }
+export default ColoradoStateParks;
